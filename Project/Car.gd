33cf215711be777d404e2960787e4cfa8439614a
Vendela Eklund <wendelize@gmail.com>
2019-10-09 12:47:21 +0200

    Animation

--- Project/Car.gd
+++ Project/Car.gd
@@ -1,28 +1,13 @@
-extends Area2D
+extends KinematicBody2D
 
-export var speed = 500
-var screenSize
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screenSize = get_viewport_rect().size
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var dir = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		dir.x += 1
-	if Input.is_action_pressed("ui_left"):
-		dir.x -= 1
-	if Input.is_action_pressed("ui_up"):
-		dir.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		dir.y += 1
-	if dir.length() > 0:
-		dir = dir.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
-	position += dir * delta
-	position.x = clamp(position.x, 0, screenSize.x)
-	position.y = clamp(position.y, 0, screenSize.y)
+#func _process(delta):
+#	pass
