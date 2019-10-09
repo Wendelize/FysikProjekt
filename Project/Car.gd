4a5c129def9ea6f3ca94453c1fb66d8e62adadbd
Vendela Eklund <wendelize@gmail.com>
2019-10-09 13:02:06 +0200

    Added 3 basic functions in script

--- Project/Car.gd
+++ Project/Car.gd
@@ -1,13 +1,32 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var wheelDist = 100
+var angle = 15
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var dir = Vector2.ZERO
+var steerAngle
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	get_input()
+	calculate_steering(delta)
+	dir = move_and_slide(dir)
+
+func get_input():
+	var turn = 0
+	if Input.is_action_pressed("steer_right"):
+		turn += 1
+	if Input.is_action_pressed("steer_left"):
+		turn -= 1
+	steerAngle = turn * deg2rad(angle)
+	dir = Vector2.ZERO
+	if Input.is_action_pressed("accalerate"):
+		dir = transform.x * 500;
+
+func calculate_steering(delta):
+	var rearWheel = position - transform.x * wheelDist / 2.0
+	var frontWheel = position + transform.x * wheelDist / 2.0
+	rearWheel += dir * delta
+	frontWheel += dir.rotated(angle) * delta
+	var newDir = (frontWheel - rearWheel).normalized()
+	dir = newDir * dir.length()
+	rotation = newDir.angle()
