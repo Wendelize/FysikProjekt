2b7c1e48555969731a567bf0acd415bc7b7ee07f
Vendela Eklund <wendelize@gmail.com>
2019-10-09 13:12:09 +0200

    Cant drive

--- Project/Car.gd
+++ Project/Car.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var wheelDist = 100
 var angle = 15
-
+var speed = 500
 var dir = Vector2.ZERO
 var steerAngle
 
@@ -13,20 +13,20 @@ func _physics_process(delta):
 
 func get_input():
 	var turn = 0
-	if Input.is_action_pressed("steer_right"):
+	if Input.is_action_pressed("ui_right"):
 		turn += 1
-	if Input.is_action_pressed("steer_left"):
+	if Input.is_action_pressed("ui_left"):
 		turn -= 1
 	steerAngle = turn * deg2rad(angle)
 	dir = Vector2.ZERO
 	if Input.is_action_pressed("accalerate"):
-		dir = transform.x * 500;
+		dir = transform.x * speed
 
 func calculate_steering(delta):
 	var rearWheel = position - transform.x * wheelDist / 2.0
 	var frontWheel = position + transform.x * wheelDist / 2.0
 	rearWheel += dir * delta
-	frontWheel += dir.rotated(angle) * delta
+	frontWheel += dir.rotated(steerAngle) * delta
 	var newDir = (frontWheel - rearWheel).normalized()
 	dir = newDir * dir.length()
 	rotation = newDir.angle()
